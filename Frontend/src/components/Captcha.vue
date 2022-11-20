91351e447bc6a370e505f3e04f48b15e57bd1c58
Jonathan <Jonathan.197ariza@gmail.com>
2022-11-20 11:01:24 -0500

    Captcha frontend added

---
+++ Frontend/src/components/Captcha.vue
@@ -0,0 +1,38 @@
+<template>
+    <VueRecaptcha
+      :sitekey="siteKey"
+      :load-recaptcha-script="true"
+      @error="handleError"
+    ></VueRecaptcha>
+</template>
+
+<script>
+import { computed, defineComponent } from 'vue';
+import { VueRecaptcha } from 'vue-recaptcha';
+
+export default defineComponent({
+  name: 'ReCaptcha',
+  components: {
+    VueRecaptcha
+  },
+  setup() {
+    const siteKey = computed(() => {
+      return import.meta.env.VITE_API_KEY_GOOGLE;
+    });
+
+    const handleError = () => {
+      // Do some validation
+    };
+
+    const handleSuccess = (response) => {
+        console.warn(response)
+    };
+
+    return {
+      handleSuccess,
+      handleError,
+      siteKey,
+    };
+  }
+});
+</script>
--- Frontend/src/views/LoginPage.vue
+++ Frontend/src/views/LoginPage.vue
@@ -1,8 +1,11 @@
 <template>
   <div
-    class="w-full mt-24 flex flex-col sm:justify-center items-center pt-6 sm:pt-0 animate__animated animate__fadeIn">
+    class="w-full mt-24 flex flex-col sm:justify-center items-center pt-6 sm:pt-0 animate__animated animate__fadeIn"
+  >
     <div class="w-full sm:max-w-md p-5 mx-auto">
-      <h2 class="mb-12 text-center text-5xl font-extrabold">Inicio de Sesión</h2>
+      <h2 class="mb-12 text-center text-5xl font-extrabold">
+        Inicio de Sesión
+      </h2>
       <form @submit.prevent="submit">
         <div class="mb-4">
           <label class="block mb-1" for="email">Correo</label>
@@ -11,7 +14,8 @@
             name="email"
             v-model="data.email"
             required
-            class="py-2 px-3 border-2 border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+            class="py-2 px-3 border-2 border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full"
+          />
         </div>
         <div class="mb-4">
           <label class="mb-1" for="password">Contraseña</label>
@@ -21,10 +25,12 @@
               name="password"
               required
               v-model="data.password"
-              class="py-2 px-3 border-l-2 border-y-2 border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-l-md shadow-sm disabled:bg-gray-100 mt-1 w-full" />
+              class="py-2 px-3 border-l-2 border-y-2 border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-l-md shadow-sm disabled:bg-gray-100 mt-1 w-full"
+            />
             <div
               @click="showPass = !showPass"
-              class="py-3 px-1 mt-1 border-y-2 border-r-2 hover:cursor-pointer border-gray-300 rounded-r-md">
+              class="py-3 px-1 mt-1 border-y-2 border-r-2 hover:cursor-pointer border-gray-300 rounded-r-md"
+            >
               <EyeOff v-if="showPass" />
               <EyeOn v-else />
             </div>
@@ -35,16 +41,31 @@
             <input
               type="checkbox"
               checked
-              class="border border-gray-300 text-red-600 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50" />
-            <label for="remember_me" class="ml-2 mt-1 block text-sm leading-5 text-gray-900">
+              class="border border-gray-300 text-red-600 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50"
+            />
+            <label
+              for="remember_me"
+              class="ml-2 mt-1 block text-sm leading-5 text-gray-900"
+            >
               Recuerdame
             </label>
           </div>
-          <router-link to="/" class="text-sm hover:underline"> ¿Has olvidado tu contraseña? </router-link>
+
+          <router-link to="/" class="text-sm hover:underline">
+            ¿Has olvidado tu contraseña?
+          </router-link>
         </div>
+
+        <div class="mt-6 flex items-center justify-between ">
+          <div class="flex items-center relative left-1/2 -translate-x-1/2 ">
+            <Captcha name="recaptcha" @verify="onVerifyRecaptcha" @expired="onExpireRecaptcha"/>
+          </div>
+        </div>
+
         <div class="mt-6">
           <button
-            class="w-full inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition">
+            class="w-full inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition"
+          >
             Iniciar Sesión
           </button>
         </div>
@@ -60,50 +81,65 @@
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue'
-import { useRouter } from 'vue-router'
-import { userStore } from '../stores/user'
-import { useToast } from 'vue-toastification'
-import { api } from '../../utils/axios'
-import EyeOn from '../components/EyeOn.vue'
-import EyeOff from '../components/EyeOff.vue'
+import { reactive, ref } from "vue";
+import { useRouter } from "vue-router";
+import { userStore } from "../stores/user";
+import { useToast } from "vue-toastification";
+import { api } from "../../utils/axios";
+import EyeOn from "../components/EyeOn.vue";
+import EyeOff from "../components/EyeOff.vue";
+import Captcha from "../components/Captcha.vue";
 
 const data = reactive({
-  email: '',
-  password: ''
-})
-const showPass = ref(false)
-const router = useRouter()
-const store = userStore()
-const toast = useToast()
-const submit = () => {
+  email: "",
+  password: "",
+  recaptcha: ""
+});
+
+const showPass = ref(false);
+const router = useRouter();
+const store = userStore();
+const toast = useToast();
+
+let tokenCaptcha = ''
+
+var onVerifyRecaptcha = (recaptchaToken) => {
+  tokenCaptcha = recaptchaToken
+}
+
+var onExpireRecaptcha = (recaptchaToken)=> {
+  tokenCaptcha = ''
+}
+
+const submit = async () => {
   api
-    .post('/login', {
+    .post("/login", {
       email: data.email,
-      password: data.password
+      password: data.password,
+      recaptcha: tokenCaptcha
     })
-    .then(response => {
-      store.$patch(state => {
-        state.isLoggedIn = true
+    .then((response) => {
+      store.$patch((state) => {
+        state.isLoggedIn = true;
         state.userInfo = {
           name: response.data.user.name,
-          email: response.data.user.email
-        }
-      })
-      router.push('/dashboard')
+          email: response.data.user.email,
+        };
+      });
+      router.push("/dashboard");
       toast.success(`${response.data.message}`, {
         timeout: 2000,
-        position: 'top-center',
-        icon: true
-      })
+        position: "top-center",
+        icon: true,
+      });
     })
-    .catch(error => {
-      console.log(error)
+    .catch((error) => {
+      console.log(error);
       toast.error(`${error.response.data.message}`, {
         timeout: 2000,
-        position: 'top-center',
-        icon: true
-      })
-    })
-}
+        position: "top-center",
+        icon: true,
+      });
+    });
+};
 </script>
