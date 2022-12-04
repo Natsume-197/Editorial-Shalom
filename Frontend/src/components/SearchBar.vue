5af7c55a2047508f0185b0bc0b1de5c8aaa3f06f
Jonathan <Jonathan.197ariza@gmail.com>
2022-12-04 15:13:46 -0500

    Update SearchBar.vue

--- Frontend/src/components/SearchBar.vue
+++ Frontend/src/components/SearchBar.vue
@@ -3,7 +3,7 @@
     <div class="x-4 py-2 w-80 h-16">
       <div class="flex-1">
         <div class="">
-          <div class="bg-white shadow-md rounded-lg px-3 py-2 mb-4">
+          <div class="bg-white absolute shadow-md rounded-lg px-3 py-2 mb-4">
             <div class="flex items-center bg-gray-200 rounded-md">
               <div class="pl-2">
                 <svg
@@ -26,13 +26,13 @@
                 v-model="query"
               />
             </div>
-            <div class="text-sm w-80 ">
+            <div class="text-sm w-80">
               <div
-                v-if="bookArray && bookArray.length > 0"
+                v-if="bookArray && bookArray.length > 0 "
                 class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2"
                 v-for="(item, index) in bookArray"
               >
-                <span class="bg-green-400 h-2 w-2 m-2 rounded-full "></span>
+                <span class="bg-green-400 h-2 w-2 m-2 rounded-full"></span>
                 <div
                   @click="itemClicked(item)"
                   class="flex-grow font-medium px-2"
@@ -85,9 +85,15 @@ export default {
       if (this.query.trim()) {
         api.get(`search?query=${this.query}`).then((response) => {
           if (response.data.books) {
-            if (response.data.books.length === 0) {
+            try {
+              if (response.data.books.length === 0) {
               this.bookArray = [];
             }
+            } catch (error) {
+              this.bookArray = [];
+              return 
+            }
+            
             var bookArray = response.data.books.slice(0, this.limit);
             this.bookArray = bookArray;
           } else {
@@ -102,7 +108,7 @@ export default {
       if (val) {
         this.resultQuery();
       } else {
-        this.bookArray = null;
+        this.bookArray = [];
       }
     },
   },
