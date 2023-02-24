0e1a643c3db77acae0adb5c9123cd958bc3e2ccb
Jonathan <Jonathan.197ariza@gmail.com>
2023-02-24 11:53:31 -0500

    Footer added

--- Frontend/src/views/principal/HomePage.vue
+++ Frontend/src/views/principal/HomePage.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Navbar from "../../components/home/NavBar.vue";
 import Showcase from "../../components/home/Showcase.vue";
+import Footer from "../../components/home/Footer.vue";
 </script>
 
 <template>
   <div class="">
     <Navbar />
     <Showcase />
+    <Footer />
   </div>
 </template>
