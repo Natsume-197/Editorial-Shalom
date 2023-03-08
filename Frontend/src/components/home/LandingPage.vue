ec05bcb5e25fcb87000dc9079bb497f6c5fd5557
Jonathan <Jonathan.197ariza@gmail.com>
2023-03-08 00:02:02 -0500

    Responsive waves

--- Frontend/src/components/home/LandingPage.vue
+++ Frontend/src/components/home/LandingPage.vue
@@ -1,174 +1,180 @@
 <template>
   <!-- hero - start -->
+  <!--Hey! This is the original version
+of Simple CSS Waves-->
 
-  <div class="waveWrapper waveAnimation -z-10">
-    <div class="waveWrapperInner bgTop">
-      <div
-        class="wave waveTop"
-        style="
-          background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png');
-        "
-      ></div>
-    </div>
-    <div class="waveWrapperInner bgMiddle">
-      <div
-        class="wave waveMiddle"
-        style="
-          background-image: url('http://front-end-noobs.com/jecko/img/wave-mid.png');
-        "
-      ></div>
-    </div>
-    <div class="waveWrapperInner bgBottom">
-      <div
-        class="wave waveBottom"
-        style="
-          background-image: url('http://front-end-noobs.com/jecko/img/wave-bot.png');
-        "
-      ></div>
-    </div>
-  </div>
-
-  <div class="pb-6 sm:pb-8 lg:pb-12 mt-32 mb-40">
-    <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
-      <section
-        class="min-h-96 flex justify-center items-center flex-1 shrink-0 bg-gray-100 overflow-hidden shadow-lg rounded-lg relative py-16 md:py-20 xl:py-48"
-      >
-        <div class="w-full lg:w-2/3 flex mb-12 md:mb-16">
-          <div
-            class="w-full h-full object-cover object-center absolute inset-0"
-          >
-            <img
-              src="../../assets/images/medium-shot-kids-laying-together.jpg"
-              class="w-full h-full object-cover object-center animate-fade-in-down"
-            />
-          </div>
-        </div>
-
-        <!-- text start -->
-        <div
-          class="sm:max-w-xl flex flex-col items-center relative p-4 bg-sky-600 rounded-l-lg shadow-xl animate-fade-in-down"
+  <div class="header">
+    <div class="pb-6 sm:pb-8 lg:pb-12 mt-32">
+      <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+        <section
+          class="min-h-96 flex justify-center items-center flex-1 shrink-0 bg-gray-100 overflow-hidden shadow-lg rounded-lg relative py-16 md:py-20 xl:py-48"
         >
-          <p
-            class="text-white text-3xl sm:text-4xl md:text-5xl font-bold text-center mb-2 md:mb-4 mt-6"
-          >
-            ¡Bienvenidos a la Editorial Shalom!
-          </p>
-          <h1 class="text-white text-lg sm:text-xl text-center mb-4 md:mb-4">
-            Libros educativos para niños en edad de preescolar
-          </h1>
-
-          <div
-            class="w-full flex flex-col sm:flex-row sm:justify-center gap-2.5 mb-4"
-          >
-            <a
-              href="#"
-              class="inline-block bg-rose-500 hover:bg-rose-600 active:bg-rose-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
-              >Comprar</a
+          <div class="w-full lg:w-2/3 flex mb-12 md:mb-16">
+            <div
+              class="w-full h-full object-cover object-center absolute inset-0"
             >
-
-            <a
-              href="#"
-              class="inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
-              >Información</a
-            >
+              <img
+                src="../../assets/images/medium-shot-kids-laying-together.jpg"
+                class="w-full h-full object-cover object-center animate-fade-in-down"
+              />
+            </div>
           </div>
-        </div>
-        <!-- text end -->
-      </section>
+
+          <!-- text start -->
+          <div
+            class="sm:max-w-xl flex flex-col items-center relative p-4 bg-sky-600 rounded-l-lg shadow-xl animate-fade-in-down"
+          >
+            <p
+              class="text-white text-3xl sm:text-4xl md:text-5xl font-bold text-center mb-2 md:mb-4 mt-6"
+            >
+              ¡Bienvenidos a la Editorial Shalom!
+            </p>
+            <h1 class="text-white text-lg sm:text-xl text-center mb-4 md:mb-4">
+              Libros educativos para niños en edad de preescolar
+            </h1>
+
+            <div
+              class="w-full flex flex-col sm:flex-row sm:justify-center gap-2.5 mb-4"
+            >
+              <a
+                href="#"
+                class="inline-block bg-rose-500 hover:bg-rose-600 active:bg-rose-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+                >Comprar</a
+              >
+
+              <a
+                href="#"
+                class="inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+                >Información</a
+              >
+            </div>
+          </div>
+          <!-- text end -->
+        </section>
+      </div>
     </div>
+    <!--Content before waves-->
+
+    <!--Waves Container-->
+    <div>
+      <svg
+        class="waves"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 24 150 28"
+        preserveAspectRatio="none"
+        shape-rendering="auto"
+      >
+        <defs>
+          <path
+            id="gentle-wave"
+            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+          />
+        </defs>
+        <g class="parallax">
+          <use
+            xlink:href="#gentle-wave"
+            x="48"
+            y="0"
+            fill="rgba(255,255,255,0.7"
+          />
+          <use
+            xlink:href="#gentle-wave"
+            x="48"
+            y="3"
+            fill="rgba(255,255,255,0.5)"
+          />
+          <use
+            xlink:href="#gentle-wave"
+            x="48"
+            y="5"
+            fill="rgba(255,255,255,0.3)"
+          />
+          <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+        </g>
+      </svg>
+    </div>
+    <!--Waves end-->
   </div>
+  <!--Header ends-->
 </template>
 
 <style>
-/***Animation Arrow down***/
-@keyframes down {
-  0% {
-    transform: translateY(0px);
-  }
-  100% {
-    transform: translateY(20px);
-  }
-}
-@-webkit-keyframes down {
-  0% {
-    -webkit-transform: translateY(0px);
-  }
-  100% {
-    -webkit-transform: translateY(20px);
-  }
+.header {
+  position: relative;
+  overflow: hidden;
+  text-align: center;
+  background: linear-gradient(
+    60deg,
+    rgb(151, 132, 235) 0%,
+    rgb(24, 164, 230) 100%
+  );
+  color: white;
 }
 
-@keyframes move_wave {
+.inner-header {
+  height: 65vh;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.waves {
+  position: relative;
+  width: 100%;
+  height: 15vh;
+  margin-bottom: -7px; /*Fix for safari gap*/
+  min-height: 100px;
+  max-height: 150px;
+}
+
+.content {
+  position: relative;
+  height: 20vh;
+  text-align: center;
+  background-color: white;
+}
+
+/* Animation */
+
+.parallax > use {
+  animation: move-forever 25s cubic-bezier(0.55, 0.5, 0.45, 0.5) infinite;
+}
+.parallax > use:nth-child(1) {
+  animation-delay: -2s;
+  animation-duration: 7s;
+}
+.parallax > use:nth-child(2) {
+  animation-delay: -3s;
+  animation-duration: 10s;
+}
+.parallax > use:nth-child(3) {
+  animation-delay: -4s;
+  animation-duration: 13s;
+}
+.parallax > use:nth-child(4) {
+  animation-delay: -5s;
+  animation-duration: 20s;
+}
+@keyframes move-forever {
   0% {
-    transform: translateX(0) translateZ(0) scaleY(1);
-  }
-  50% {
-    transform: translateX(-25%) translateZ(0) scaleY(0.55);
+    transform: translate3d(-90px, 0, 0);
   }
   100% {
-    transform: translateX(-50%) translateZ(0) scaleY(1);
+    transform: translate3d(85px, 0, 0);
   }
 }
-.waveWrapper {
-  overflow: hidden;
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  top: 0;
-  margin: auto;
-}
-.waveWrapperInner {
-  position: absolute;
-  width: 100%;
-  overflow: hidden;
-  height: 100%;
-  bottom: -1px;
-  background-image: linear-gradient(
-    to top,
-    rgb(14 165 233) 20%,
-    rgb(14 165 233) 80%
-  );
-}
-.bgTop {
-  z-index: 15;
-  opacity: 0.5;
-}
-.bgMiddle {
-  z-index: 10;
-  opacity: 0.75;
-}
-.bgBottom {
-  z-index: 5;
-}
-.wave {
-  position: absolute;
-  left: 0;
-  width: 200%;
-  height: 100%;
-  background-repeat: repeat no-repeat;
-  background-position: 0 bottom;
-  transform-origin: center bottom;
-}
-.waveTop {
-  background-size: 50% 100px;
-}
-.waveAnimation .waveTop {
-  animation: move-wave 3s;
-  -webkit-animation: move-wave 3s;
-  -webkit-animation-delay: 1s;
-  animation-delay: 1s;
-}
-.waveMiddle {
-  background-size: 50% 120px;
-}
-.waveAnimation .waveMiddle {
-  animation: move_wave 10s linear infinite;
-}
-.waveBottom {
-  background-size: 50% 100px;
-}
-.waveAnimation .waveBottom {
-  animation: move_wave 15s linear infinite;
+/*Shrinking for mobile*/
+@media (max-width: 768px) {
+  .waves {
+    height: 40px;
+    min-height: 40px;
+  }
+  .content {
+    height: 30vh;
+  }
+  h1 {
+    font-size: 24px;
+  }
 }
 </style>
